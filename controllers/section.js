e8032222ccd5f1f8c23c6057546287e47a0ad8d8
Manisha <manisha.mengani468@gmail.com>
2019-11-08 17:02:53 -0600

    section module changes

--- controllers/section.js
+++ controllers/section.js
@@ -30,89 +30,91 @@ api.get('/findone/:id', (req, res) => {
   res.send(JSON.stringify(item))
 })
 
-// // RESPOND WITH VIEWS  --------------------------------------------
+// RESPOND WITH VIEWS  --------------------------------------------
 
-// // GET to this controller base URI (the default)
-// api.get('/', (req, res) => {
-//   res.render('developer/index.ejs', {
-//     developers: req.app.locals.sections.query
-//   })
-// })
+// GET to this controller base URI (the default)
+api.get('/', (req, res) => {
+  res.render('section/index.ejs', {
+    sections: req.app.locals.sections.query
+  })
+})
 
-// // GET create
-// api.get('/create', (req, res) => {
-//   res.render('developer/create', {
-//     developers: req.app.locals.developers.query,
-//     developer: new Model()
-//   })
-// })
+// GET create
+api.get('/create', (req, res) => {
+  res.render('section/create', {
+    sections: req.app.locals.sections.query,
+    section: new Model()
+  })
+})
 
-// // GET /delete/:id
-// api.get('/delete/:id', (req, res) => {
-//   const id = parseInt(req.params.id)
-//   const data = req.app.locals.developers.query
-//   const item = find(data, { _id: id })
-//   if (!item) { return res.end(notfoundstring + id) }
-//   res.render('developer/delete', {
-//     developer: item
-//   })
-// })
+// GET /delete/:id
+api.get('/delete/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const data = req.app.locals.sections.query
+  const item = find(data, { _id: id })
+  if (!item) { return res.end(notfoundstring + id) }
+  res.render('section/delete', {
+    section: item
+  })
+})
 
-// // GET /details/:id
-// api.get('/details/:id', (req, res) => {
-//   const id = parseInt(req.params.id)
-//   const data = req.app.locals.developers.query
-//   const item = find(data, { _id: id })
-//   if (!item) { return res.end(notfoundstring + id) }
-//   res.render('developer/details', {
-//     developer: item
-//   })
-// })
+// GET /details/:id
+api.get('/details/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const data = req.app.locals.sections.query
+  const item = find(data, { _id: id })
+  if (!item) { return res.end(notfoundstring + id) }
+  res.render('section/details', {
+    section: item
+  })
+})
 
-// // GET one
-// api.get('/edit/:id', (req, res) => {
-//   const id = parseInt(req.params.id)
-//   const data = req.app.locals.developers.query
-//   const item = find(data, { _id: id })
-//   if (!item) { return res.end(notfoundstring + id) }
-//   res.render('developer/edit', {
-//     developer: item
-//   })
-// })
+// GET one
+api.get('/edit/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const data = req.app.locals.sections.query
+  const item = find(data, { _id: id })
+  if (!item) { return res.end(notfoundstring + id) }
+  res.render('section/edit', {
+    section: item
+  })
+})
 
-// // HANDLE EXECUTE DATA MODIFICATION REQUESTS --------------------------------------------
+// HANDLE EXECUTE DATA MODIFICATION REQUESTS --------------------------------------------
 
-// // POST new
-// api.post('/save', (req, res) => {
-//   console.info(`Handling POST ${req}`)
-//   console.debug(JSON.stringify(req.body))
-//   const item = new Model()
-//   console.info(`NEW ID ${req.body._id}`)
-//   item._id = parseInt(req.body._id)
-//   item.email = req.body.email
-//   item.given = req.body.given
-//   item.family = req.body.family
-//   item.city = req.body.city
-//   item.state = req.body.state
-//   item.zip = req.body.zip
-//   item.country = req.body.country
-//   res.send(`THIS FUNCTION WILL SAVE A NEW developer ${JSON.stringify(item)}`)
-// })
+// POST new
 
-// // POST update with id
-// api.post('/save/:id', (req, res) => {
-//   console.info(`Handling SAVE request ${req}`)
-//   const id = parseInt(req.params.id)
-//   console.info(`Handling SAVING ID=${id}`)
-//   res.send(`THIS FUNCTION WILL SAVE CHANGES TO AN EXISTING developer with id=${id}`)
-// })
 
-// // DELETE id (uses HTML5 form method POST)
-// api.post('/delete/:id', (req, res) => {
-//   console.info(`Handling DELETE request ${req}`)
-//   const id = parseInt(req.params.id)
-//   console.info(`Handling REMOVING ID=${id}`)
-//   res.send(`THIS FUNCTION WILL DELETE FOREVER THE EXISTING developer with id=${id}`)
-// })
+api.post('/save', (req, res) => {
+    console.info(`Handling POST ${req}`)
+    console.debug(JSON.stringify(req.body))
+    const item = new Model()
+    console.info(`NEW ID ${req.body._id}`)
+    item._id = parseInt(req.body._id)
+    item.sectionNo  = req.body.sectionNumber 
+    item.Days = req.body.Days
+    item.StartTime = req.body.StartTime
+    item.RoomNumber = req.body.RoomNumber
+    item.CourseID = req.body.CourseID
+    res.send(`THIS FUNCTION WILL SAVE A NEW Section ${JSON.stringify(item)}`)
+  }) 
+   
+  
+
+// POST update with id
+api.post('/save/:id', (req, res) => {
+  console.info(`Handling SAVE request ${req}`)
+  const id = parseInt(req.params.id)
+  console.info(`Handling SAVING ID=${id}`)
+  res.send(`THIS FUNCTION WILL SAVE CHANGES TO AN EXISTING section with id=${id}`)
+})
+
+// DELETE id (uses HTML5 form method POST)
+api.post('/delete/:id', (req, res) => {
+  console.info(`Handling DELETE request ${req}`)
+  const id = parseInt(req.params.id)
+  console.info(`Handling REMOVING ID=${id}`)
+  res.send(`THIS FUNCTION WILL DELETE FOREVER THE EXISTING section with id=${id}`)
+})
 
 module.exports = api
